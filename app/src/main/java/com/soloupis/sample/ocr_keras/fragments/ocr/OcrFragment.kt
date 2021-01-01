3902d8e8919fae2fd9f80e5e5f5aa319e528a96f
George <farmaker47@gmail.com>
2021-01-01 07:31:59 +0200

    Using ML kit for text recognition..output at logs

--- app/src/main/java/com/soloupis/sample/ocr_keras/fragments/ocr/OcrFragment.kt
+++ app/src/main/java/com/soloupis/sample/ocr_keras/fragments/ocr/OcrFragment.kt
@@ -36,7 +36,7 @@ import java.util.*
  * This is where we show both the captured input image and the output image
  */
 class OcrFragment : Fragment(),
-        SearchFragmentNavigationAdapter.SearchClickItemListener {
+    SearchFragmentNavigationAdapter.SearchClickItemListener {
 
     // Koin inject ViewModel
     private val viewModel: OcrViewModel by viewModel()
@@ -46,7 +46,6 @@ class OcrFragment : Fragment(),
 
     // RecyclerView
     private lateinit var searchFragmentNavigationAdapter: SearchFragmentNavigationAdapter
-    private lateinit var ocrModelExecutor: OcrModelExecutor
     private var outputArray: LongArray = longArrayOf()
     private var inferenceFullTime = 0L
     private lateinit var labels: List<String>
@@ -58,8 +57,8 @@ class OcrFragment : Fragment(),
     }
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentOcrFragmentBinding.inflate(inflater)
@@ -68,21 +67,18 @@ class OcrFragment : Fragment(),
 
         // RecyclerView setup
         searchFragmentNavigationAdapter =
-                SearchFragmentNavigationAdapter(
-                        requireActivity(),
-                        viewModel.currentList,
-                        this
-                )
+            SearchFragmentNavigationAdapter(
+                requireActivity(),
+                viewModel.currentList,
+                this
+            )
         binding.recyclerViewImages.apply {
             setHasFixedSize(true)
             layoutManager =
-                    LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+                LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
             adapter = searchFragmentNavigationAdapter
 
         }
-
-        // Initialize class with Koin
-        ocrModelExecutor = get()
 
         observeViewModel()
 
@@ -95,13 +91,20 @@ class OcrFragment : Fragment(),
 
         // Observe ocr procedure
         viewModel.inferenceDone.observe(
-                requireActivity(),
-                Observer { loadingDone ->
-                    when (loadingDone) {
-                        //true -> binding.progressbarStyle.visibility = View.GONE
-                    }
+            requireActivity(),
+            Observer { loadingDone ->
+                when (loadingDone) {
+                    //true -> binding.progressbarStyle.visibility = View.GONE
                 }
+            }
         )
+
+        // Observe ML kit inference time
+        viewModel.inferenceTimeMlKit.observe(requireActivity(), Observer { time ->
+
+            Log.e("ML_Kit_time", time.toString())
+
+        })
 
     }
 
@@ -111,35 +114,37 @@ class OcrFragment : Fragment(),
 
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        // clean up coroutine job
-    }
-
     companion object {
         private const val TAG = "OcrFragment"
         private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
-        const val MODEL_WIDTH = 256
-        const val MODEL_HEIGHT = 256
     }
 
     override fun onListItemClick(itemIndex: Int, sharedImage: ImageView?, imagePath: String) {
 
+        // Using Keras OCR
         executeOcr(imagePath)
         //getKoin().setProperty(getString(R.string.koinStyle), type)
-        binding.imageviewOutput.setImageBitmap(getBitmapFromAsset(requireActivity(), "thumbnails/$imagePath"))
+        binding.imageviewOutput.setImageBitmap(
+            getBitmapFromAsset(
+                requireActivity(),
+                "thumbnails/$imagePath"
+            )
+        )
+
+        // Using ML Kit
+        viewModel.performOcrWithMlKit(imagePath, requireActivity())
 
     }
 
     private fun getBitmapFromAsset(context: Context, path: String): Bitmap =
-            context.assets.open(path).use { BitmapFactory.decodeStream(it) }
+        context.assets.open(path).use { BitmapFactory.decodeStream(it) }
 
     private fun executeOcr(path: String): Pair<LongArray, Long> {
 
         lifecycleScope.launch(Dispatchers.Default) {
 
             val (longArray, inferenceTime) = viewModel.onClickPerformOcr(
-                    requireActivity(), path
+                requireActivity(), path
             )
             withContext(Dispatchers.Main) {
                 outputArray = longArray
--- app/src/main/java/com/soloupis/sample/ocr_keras/fragments/ocr/OcrViewModel.kt
+++ app/src/main/java/com/soloupis/sample/ocr_keras/fragments/ocr/OcrViewModel.kt
@@ -2,23 +2,28 @@ package com.soloupis.sample.ocr_keras.fragments.ocr
 
 import android.app.Application
 import android.content.Context
-import android.graphics.*
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.SystemClock
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
 import org.koin.core.KoinComponent
 import org.koin.core.get
 import java.io.IOException
 
 class OcrViewModel(application: Application) :
-        AndroidViewModel(application),
-        KoinComponent {
+    AndroidViewModel(application),
+    KoinComponent {
 
     private lateinit var scaledMaskBitmap: Bitmap
     private lateinit var outputArray: LongArray
     var startTime: Long = 0L
+    var startTimeMlKit: Long = 0L
+
     var inferenceTime = 0L
 
     var seekBarProgress: Float = 0F
@@ -31,6 +36,10 @@ class OcrViewModel(application: Application) :
     val inferenceDone: LiveData<Boolean>
         get() = _inferenceDone
 
+    private val _inferenceTimeMlKit = MutableLiveData<Long>()
+    val inferenceTimeMlKit: LiveData<Long>
+        get() = _inferenceTimeMlKit
+
     val ocrModelExecutor: OcrModelExecutor
 
     init {
@@ -42,11 +51,11 @@ class OcrViewModel(application: Application) :
     }
 
     fun onClickPerformOcr(
-            context: Context,
-            imageFilePath: String
+        context: Context,
+        imageFilePath: String
     ): Pair<LongArray, Long> {
 
-        return performOcrWithImage(imageFilePath,context)
+        return performOcrWithImage(imageFilePath, context)
     }
 
     fun performOcrWithImage(thumbnailsImageName: String, context: Context): Pair<LongArray, Long> {
@@ -55,7 +64,12 @@ class OcrViewModel(application: Application) :
             startTime = SystemClock.uptimeMillis()
 
             // Run inference
-            val result = ocrModelExecutor.executeOcrWithInterpreter(getBitmapFromAsset(context,"thumbnails/$thumbnailsImageName"), context)
+            val result = ocrModelExecutor.executeOcrWithInterpreter(
+                getBitmapFromAsset(
+                    context,
+                    "thumbnails/$thumbnailsImageName"
+                ), context
+            )
             outputArray = result
 
             inferenceTime = SystemClock.uptimeMillis() - startTime
@@ -67,8 +81,39 @@ class OcrViewModel(application: Application) :
         return Pair(outputArray, inferenceTime)
     }
 
+    fun performOcrWithMlKit(thumbnailsImageName: String, context: Context) {
+
+        startTimeMlKit = System.currentTimeMillis()
+
+        val scaledBitmap = Bitmap.createScaledBitmap(
+            getBitmapFromAsset(
+                context,
+                "thumbnails/$thumbnailsImageName"
+            ),
+            200,
+            32,
+            true
+        )
+
+        val image = InputImage.fromBitmap(
+            scaledBitmap, 0
+        )
+        val recognizer = TextRecognition.getClient()
+        recognizer.process(image)
+            .addOnSuccessListener { texts ->
+                Log.e("ML_Kit", texts.text)
+
+                _inferenceTimeMlKit.value = System.currentTimeMillis() - startTimeMlKit
+            }
+            .addOnFailureListener { e -> // Task failed with an exception
+                e.printStackTrace()
+            }
+
+
+    }
+
     private fun getBitmapFromAsset(context: Context, path: String): Bitmap =
-            context.assets.open(path).use { BitmapFactory.decodeStream(it) }
+        context.assets.open(path).use { BitmapFactory.decodeStream(it) }
 
 
     override fun onCleared() {
